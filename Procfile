2bcb33090654310d80f8f8779e98fd3d3b085710
pablolx <pablo.pinheiro@gmail.com>
2025-11-18 16:43:33 -0300

    Update Procfile and settings for static files; modify footer and index content

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn novoPortfolio.wsgi
+
+python manage.py collectstatic --noinput
--- novoPortfolio/settings.py
+++ novoPortfolio/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "website/static",]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -153,6 +153,8 @@ MEDIA_ROOT = BASE_DIR / "staticfiles/img"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGIN_REDIRECT_URL = '/home'
