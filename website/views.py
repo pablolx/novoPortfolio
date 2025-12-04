c7b008156771bd183e23848a018da329074e5f8a
pablolx <pablo.pinheiro@gmail.com>
2025-12-04 10:58:12 -0300

    Changes to be committed: modified:   templates/novoPortfolio/projetos_list.html modified:   website/static/css/style.css modified:   website/views.py

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy    
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Project
 
@@ -9,20 +10,21 @@ from .models import Project
 class ProjetosListView(ListView):
     model = Project
     template_name = 'novoPortfolio/projetos_list.html'
+    paginate_by = 12
 
-class ProjetosCreateView(CreateView):
+class ProjetosCreateView(LoginRequiredMixin, CreateView):
     model = Project
     template_name = 'novoPortfolio/projetos_form.html'
     fields = ['title', 'description', 'repo_url', 'live_url', 'tech_stack', ]
     success_url = reverse_lazy('projetos_list')
 
-class ProjetosUpdateView(UpdateView):
+class ProjetosUpdateView(LoginRequiredMixin, UpdateView):
     model = Project
     template_name = 'novoPortfolio/projetos_form.html'
     fields = ['title', 'description', 'repo_url', 'live_url', 'tech_stack', ]
     success_url = reverse_lazy('projetos_list')
 
-class ProjetosDeleteView(DeleteView):
+class ProjetosDeleteView(LoginRequiredMixin, DeleteView):
     model = Project
     template_name = 'novoPortfolio/projetos_confirm_delete.html'
     success_url = reverse_lazy('projetos_list')
